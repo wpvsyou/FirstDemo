0c57c030ec28477c5d9f49926467ac7fb50f15b2
王鹏 <peng.wang@pekall.com>
2014-09-05 16:09:30 +0800

    add some files of gradle

---
+++ build/source/buildConfig/debug/net/sf/andpdf/pdfviewer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.sf.andpdf.pdfviewer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "net.sf.andpdf.pdfviewer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 19;
+}
